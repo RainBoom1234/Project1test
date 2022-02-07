808aca8248646c3762726b6dda8b9c4682376630
RainBoom1234 <rain.mannimets@vikk.ee>
2022-02-07 14:23:00 +0000

    epic hacking code engaged B) [FBI approves]

--- script1.sh
+++ script1.sh
@@ -3,3 +3,14 @@ echo "Töös huuga, tehas, vili, nurmel vooga,"
 echo "sirp, lõika, alasile, haamer, löö!"
 date +%d_%m_%Y_ 
 ls -l
+
+sleep 1
+echo "code; hack pewdiepie :sunglasses_emoij:; commenced;"
+sleep 3
+echo "hacking in progress"
+sleep 7
+echo "acsess denied :("
+sleep 2
+echo "re-hacking the mainframe"
+sleep 4
+echo "hacking sucsessful B)"
